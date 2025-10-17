f0009d2557ee33a5d05a10b6bb60f366fb04b762
robotescu <80849623+robotescu@users.noreply.github.com>
2025-10-17 19:48:26 +0300

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,11 @@
 basic.pause(2000)
-pins.setEvents(DigitalPin.P14, PinEventType.Touch)
+pins.setEvents(DigitalPin.P8, PinEventType.None)
+pins.digitalWritePin(DigitalPin.P8, 1)
 basic.showIcon(IconNames.Happy)
 basic.forever(function () {
     if (pins.analogReadPin(AnalogPin.P0) > 500) {
-        pins.digitalWritePin(DigitalPin.P14, 0)
+        pins.digitalWritePin(DigitalPin.P8, 0)
         basic.pause(100)
-        pins.digitalWritePin(DigitalPin.P14, 1)
+        pins.digitalWritePin(DigitalPin.P8, 1)
     }
 })
